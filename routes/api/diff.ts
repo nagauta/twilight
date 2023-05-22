3092785b7a7f9d0dae452dc86d69b721db6669c3
mie035 <hello@michitoshiyano.com>
2023-05-23 01:33:40 +0900

    update: date format

--- routes/api/diff.ts
+++ routes/api/diff.ts
@@ -4,9 +4,8 @@ import {config} from "../../config.ts";
 
 export const handler = async (_req: Request, _ctx: HandlerContext): Promise<Response> => {
 
-  // const now = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
-  const now = formatDate(new Date());
-  console.log(`diff start: ${now}`);
+  const now = new Date();
+  console.log(`diff start: ${now.toISOString()}`);
   const current = await fetch("https://mask-api.icloud.com/egress-ip-ranges.csv");
   const textCurrent = await current.text();
   const hashCurrent = await getHash(textCurrent);
@@ -17,7 +16,7 @@ export const handler = async (_req: Request, _ctx: HandlerContext): Promise<Resp
   console.log(`current: ${hashCurrent}, stored: ${hashStored}`);
   
   const headers = { "content-type": "application/json" };
-  const json = { isSameCsv, datetimeCurrent: now, datetimeStored: config.egressIpRangesCsvStoredDate};
+  const json = { isSameCsv, datetimeCurrent: now.toISOString(), datetimeStored: config.egressIpRangesCsvStoredDate};
   return new Response(JSON.stringify(json), { headers });
 };
 async function getHash(s: string) {
@@ -25,20 +24,3 @@ async function getHash(s: string) {
   const digest = await crypto.subtle.digest("sha-256", data.buffer);
   return encodeToString(new Uint8Array(digest));
 }
-function formatDate(dt:Date) {
-  const yyyymmdd = new Intl.DateTimeFormat(
-    undefined,
-    {
-      year:   'numeric',
-      month:  '2-digit',
-      day:    '2-digit',
-      hour:   '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    }
-  )
-  
-  return yyyymmdd.format(dt);
-}
-
-
