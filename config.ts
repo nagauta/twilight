4865efc7a5374adddde01e32b05fe8cbd8b50a4f
mie035 <hello@michitoshiyano.com>
2023-05-26 22:07:22 +0900

    update config

--- config.ts
+++ config.ts
@@ -1,5 +1,10 @@
+import "https://deno.land/std@0.188.0/dotenv/load.ts";
+
 interface Config {
     egressIpRangesCsvStoredDate: string;
+    DATABASE_URL: string;
   }
   
-  export const config: Config = JSON.parse(await Deno.readTextFile("./deno.json")).config;
+  const json = JSON.parse(await Deno.readTextFile("./deno.json")).config;
+  json.DATABASE_URL = Deno.env.get("DATABASE_URL");
+  export const config: Config = json;
